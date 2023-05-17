9720000e8db2632158c81bef26fdf97bfb8ead51
Brenda <73964041+Brenda403@users.noreply.github.com>
2023-05-17 14:27:36 -0400

    fixed last test function

--- __tests__/duelDuo.test.js
+++ __tests__/duelDuo.test.js
@@ -15,4 +15,35 @@ describe("Duel Duo tests", () => {
     await driver.get("http://localhost:8000");
     await driver.wait(until.titleIs("Duel Duo"), 1000);
   });
+  test("clicking the Draw button displays the div with id = choices", async () => {
+    await driver.get("http://localhost:8000");
+    //press the draw button
+    const drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+    // wait for choices div to be displayed
+    await driver.wait(until.elementLocated(By.id("choices")), 1000);
+    const choicesDiv = await driver.findElement(By.id("choices"));
+    // what is found should match the choices div section
+    const idAttribute = await choicesDiv.getAttribute("id");
+    expect(idAttribute).toBe("choices");
+  });
+  test("clicking an “Add to Duo” button displays the div with id = player-duo", async () => {
+    // navigate to page
+    await driver.get("http://localhost:8000");
+    // click draw button
+    const drawButton = await driver.findElement(By.id("draw"));
+    await drawButton.click();
+    // wait for choices div to be displayed
+    await driver.wait(until.elementLocated(By.id("choices")), 5000);
+    //find first button and click
+    const addToDuoButton = await driver.findElements(By.css(".bot-btn"));
+    const firstAddToDuoButton = addToDuoButton[0];
+    await driver.executeScript("arguments[0].click();", firstAddToDuoButton);
+
+    await driver.wait(until.elementLocated(By.id("player-duo")), 1000);
+    const playerDuoDiv = await driver.findElement(By.id("player-duo"));
+
+    const idAttribute = await playerDuoDiv.getAttribute("id");
+    expect(idAttribute).toBe("player-duo");
+  });
 });
--- __tests__/shuffle.test.js
+++ __tests__/shuffle.test.js
@@ -1,5 +1,14 @@
 const shuffle = require("../src/shuffle");
 
 describe("shuffle should...", () => {
-  // CODE HERE
+  test("check that shuffle returns an array", () => {
+    //array.isArray() ... to check if what shuffle returns is an array?
+    const result = shuffle([1, 2, 3]);
+    expect(Array.isArray(result)).toBe(true);
+  });
+  test("check that it returns an array of the same length as the argument sent in", () => {
+    const input = [1, 2, 3, 4, 5];
+    const result = shuffle(input);
+    expect(result.length).toBe(input.length);
+  });
 });
