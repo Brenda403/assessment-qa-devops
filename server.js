7cd1f590a5bbad329e2b1bab4c319c4cfd837013
Brenda <73964041+Brenda403@users.noreply.github.com>
2023-05-17 18:27:15 -0400

    added rollbar and attempted deployment connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bots = require("./src/botsData");
 const shuffle = require("./src/shuffle");
@@ -14,6 +15,32 @@ const app = express();
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
+
+const { ROLLBAR_ACCESS_TOKEN } = process.env;
+
+//for deployment
+// app.use(express.static(`${__dirname}/public`))
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: `${ROLLBAR_ACCESS_TOKEN}`,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+//ROLLBAR docs
+
+// reports a string message at the specified level, along with a request and callback
+// only the first param is required
+// rollbar.debug("Response time exceeded threshold of 1s", request, callback);
+// rollbar.info("Response time exceeded threshold of 1s", request, callback);
+// rollbar.warning("Response time exceeded threshold of 1s", request, callback);
+// rollbar.error("Response time exceeded threshold of 1s", request, callback);
+// rollbar.critical("Response time exceeded threshold of 1s", request, callback);
 
 // Add up the total health of all the robots
 const calculateTotalHealth = (robots) =>
@@ -44,6 +71,7 @@ app.get("/api/robots", (req, res) => {
   try {
     res.status(200).send(botsArr);
   } catch (error) {
+    rollbar.error("Error handling '/api/robots' endpoint", error);
     console.error("ERROR GETTING BOTS", error);
     res.sendStatus(400);
   }
@@ -52,6 +80,7 @@ app.get("/api/robots", (req, res) => {
 app.get("/api/robots/shuffled", (req, res) => {
   try {
     let shuffled = shuffle(bots);
+    rollbar.info("Robots list shuffled");
     res.status(200).send(shuffled);
   } catch (error) {
     console.error("ERROR GETTING SHUFFLED BOTS", error);
@@ -74,6 +103,7 @@ app.post("/api/duel", (req, res) => {
       res.status(200).send("You lost!");
     } else {
       playerRecord.losses += 1;
+      rollbar.info("Player won a duel");
       res.status(200).send("You won!");
     }
   } catch (error) {
@@ -84,8 +114,10 @@ app.post("/api/duel", (req, res) => {
 
 app.get("/api/player", (req, res) => {
   try {
+    rollbar.log(`player record is ${playerRecord}`);
     res.status(200).send(playerRecord);
   } catch (error) {
+    rollbar.critical("player record has encountered issues");
     console.log("ERROR GETTING PLAYER STATS", error);
     res.sendStatus(400);
   }
